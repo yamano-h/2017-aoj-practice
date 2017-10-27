fe857afbbf6ab284920f255c73c4760cfe6457c5
山野秀哲 <Hidenori@Hidenori.local>
2017-10-27 12:42:01 +0900

    Test

---
+++ AizuOnlineJudge/src/com/plactice/yamano_h/q0011/Main.java
@@ -0,0 +1,29 @@
+//package com.plactice.yamano_h.q0011;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.Objects;
+//
+//class Main {
+//	/**
+//	 * 入力の読み取り / 回答の出力を担う
+//	 */
+//	public static void main(String[] args) throws Exception {
+//
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+////		String answer = getScore(br);
+//
+//		System.out.println(answer);
+//	}
+//
+//
+//	/**
+//	 * 終了条件を判断する
+//	 */
+//	private static boolean isEndLine(String line) {
+//
+//		return Objects.isNull(line);
+//	}
+//}
